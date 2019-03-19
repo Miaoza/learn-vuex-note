b5ac6a3b0c51c51d19b8b5868a5ef10caf3ac63d
Nianko <634410121@qq.com>
2019-03-19 17:07:15 +0800

    vuex code and some learn note

--- README.md
+++ README.md
@@ -1,5 +1,23 @@
 ###
 
-learn vuex some notes
+learn vuex code some notes
+vuex v3.1.0
+
+###
+
+###
+
+import Vue from 'vue';
+import Vuex from 'vuex';
+
+Vue.use(Vuex); // 调用插件的 install 方法，传入 Vue 对象
+Vue.js 提供 Vue.use 方法来给 Vue.js 安装插件，
+通过调用插件的 install 方法安装插件
+
+将 store 放入 Vue 创建时的 option 中
+new Vue({
+el: '#app',
+store // 在 beforeCreated 钩子中会用到
+});
 
 ###
---
+++ src/mixin.js
@@ -0,0 +1,39 @@
+/**
+ * 给 Vue 的实例注入一个 $store 的属性
+ * @param {*} Vue Vue 对象
+ */
+export default function(Vue) {
+  const version = Number(Vue.version.split('.')[0])
+  // 判断版本号是否>2
+  if (version >= 2) {
+    // 将 vuexInit 混入 beforeCreated
+    Vue.mixin({ beforeCreate: vuexInit })
+  } else {
+    // override init and inject vuex init procedure
+    // for 1.x backwards compatibility.
+    // 将 vuexInit 放入 Vue 的 _init 方法中
+    const _init = Vue.prototype._init
+    Vue.prototype._init = function(options = {}) {
+      options.init = options.init ? [vuexInit].concat(options.init) : vuexInit
+      _init.call(this, options)
+    }
+  }
+
+  /**
+   * Vuex init hook, injected into each instances init hooks list.
+   */
+  // Vuex 初始化代码，根组件从这里拿到 store，子组件从父组件拿到 store
+  function vuexInit() {
+    const options = this.$options
+    // store injection
+    // 如果当前组件是根组件，会从 Vue 的 options 获取 store
+    // 如果不是 root，会从父组件获取 store
+    // 所有组件的 store 实例指向了同一个内存地址
+    if (options.store) {
+      this.$store =
+        typeof options.store === 'function' ? options.store() : options.store
+    } else if (options.parent && options.parent.$store) {
+      this.$store = options.parent.$store
+    }
+  }
+}
